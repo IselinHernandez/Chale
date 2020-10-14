673dd818e332dd4e9f438bbd1c9c2bd40578c31a
Iselin Olguin <you@example.com>
2020-10-13 19:18:34 -0500

    agrego el primer archivo

---
+++ C13.py
@@ -0,0 +1,9 @@
+def areYouPlayingBanjo (name):
+	name=("Are you playing banjo?")
+	if name [0] == "R"  or name [0] == "r" :
+		name = name +"you are playing banjo"
+	else:
+		name= name + "does not play banjo"
+
+		return name
+
